4f35bd80102ad4070e7cc9429f0d769649ce58da
osman <osman@osman123.com>
2021-09-07 09:37:03 +0300

    day 2 1

--- javaproject/src/com/java/training/lambda/LambdaRun.java
+++ javaproject/src/com/java/training/lambda/LambdaRun.java
@@ -3,6 +3,7 @@ package com.java.training.lambda;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class LambdaRun {
 
@@ -14,8 +15,14 @@ public class LambdaRun {
         System.out.println(f.apply("osman"));
 
         List<String> listLoc = Arrays.asList("osman",
+                                             "osman",
+                                             "ahmet",
+                                             "osman",
+                                             "mehmet",
                                              "ali",
+                                             "mehmet",
                                              "veli",
+                                             "mehmet",
                                              "ayşe");
         for (String stringLoc : listLoc) {
             System.out.println(stringLoc);
@@ -24,5 +31,42 @@ public class LambdaRun {
         listLoc.forEach(z -> System.out.println(z));
 
         listLoc.forEach(System.out::println);
+
+        System.out.println("-------------1-------------");
+        listLoc.stream()
+               .filter(d -> d.length() > 3)
+               .distinct()
+               .sorted()
+               .forEach(System.out::println);
+        System.out.println("-------------2-------------");
+        listLoc.stream()
+               .distinct()
+               .sorted()
+               .parallel()
+               .filter(d -> d.length() > 3)
+               .peek(o -> System.out.println("Thread : "
+                                             + Thread.currentThread()
+                                                     .getName()
+                                             + " "
+                                             + o))
+               .map(r -> r.length())
+               .forEach(System.out::println);
+        System.out.println("-------------3-------------");
+        listLoc.stream()
+               .distinct()
+               .sorted()
+               .filter(d -> d.length() > 3)
+               .map(r -> r.length())
+               .forEach(System.out::println);
+
+        System.out.println("-------------3-------------");
+        List<String> collectLoc = listLoc.stream()
+                                         .distinct()
+                                         .sorted()
+                                         .filter(d -> d.length() > 3)
+                                         .collect(Collectors.toList());
+        collectLoc.forEach(System.out::println);
+
+
     }
 }
